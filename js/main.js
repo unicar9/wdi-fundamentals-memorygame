ca53d28c33cc9d8cb7c27310e203c1c4a41d4586
Unicar <Unicar@Macintosh.local>
2017-05-03 20:49:10 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 console.log("Up and running!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log('User flipped ' + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
