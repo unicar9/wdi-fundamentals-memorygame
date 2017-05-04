4677438191824c7e7257737c8a0661265db2cd05
Unicar <Unicar@Macintosh.local>
2017-05-04 11:57:49 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,22 +3,29 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log('User flipped ' + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
 }
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}	
+}
+
+flipCard(0);
+flipCard(1);
+
+
+
+
+
+
+
