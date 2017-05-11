65159efe39ddae24b56a1bb74b1de651ba4f1c82
Unicar <Unicar@Macintosh.local>
2017-05-11 14:56:41 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -26,27 +26,40 @@ var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
   } else {
-      console.log("Sorry, try again.");
+      alert("Sorry, try again.");
   }
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
+
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}	
 }
 
-flipCard(0);
-flipCard(1);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
 
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
+createBoard ();
 
 
 
